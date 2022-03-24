87c8a1e9029f422031f0d1b0586eb7d77573e83d
Kristen Scheel <kristen.scheel@gmail.com>
2022-03-24 09:37:46 -0400

    initial commit

---
+++ data.js
@@ -0,0 +1,42 @@
+let data = [
+  {
+    city: "Vancouver",
+    province: "British Columbia",
+    months: [2, 1, 4, 6, 9, 12, 14, 14, 12, 7, 3, 1],
+  },
+  {
+    city: "Victoria",
+    province: "British Columbia",
+    months: [3, 3, 4, 6, 8, 10, 11, 12, 10, 7, 4, 3],
+  },
+  {
+    city: "Kelowna",
+    province: "British Columbia",
+    months: [-5, -6, -2, 1, 6, 10, 12, 11, 7, 2, -2, -5],
+  },
+  {
+    city: "Toronto",
+    province: "Ontario",
+    months: [-7, -6, -2, 3, 11, 15, 18, 18, 14, 8, 2, -3],
+  },
+  {
+    city: "Ottawa",
+    province: "Ontario",
+    months: [-13, -13, -6, 1, 9, 13, 16, 15, 11, 5, -2, -8],
+  },
+  {
+    city: "Montreal",
+    province: "Quebec",
+    months: [-13, -11, -6, 1, 9, 14, 17, 16, 12, 5, -2, -8],
+  },
+  {
+    city: "Halifax",
+    province: "Nova Scotia",
+    months: [-7, -7, -4, 1, 6, 10, 15, 15, 12, 7, 1, -3],
+  },      
+  {
+    city: "St. John's",
+    province: "NewFoundland",
+    months: [-6, -7, -6, -2, 2, 6, 12, 13, 9, 4, 0, -4],
+  },
+];
